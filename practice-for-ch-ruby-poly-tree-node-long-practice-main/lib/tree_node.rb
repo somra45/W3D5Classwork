947c94eabf801406fee91b57bc01e3c5320224a7
root <somra45@gmail.com>
2023-03-24 08:57:21 -0400

    initialize node class, add parent= method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
-  
+    attr_reader :parent, :children, :value
+
+    def initialize(value = nil)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        if self.parent != nil 
+            self.parent.children.reject! {|ele| ele == self}
+        end
+        @parent = parent_node
+        if parent_node != nil &&  !parent_node.children.include?(self)
+            parent_node.children << self 
+            
+        end
+
+    end
+
+    # def add_child
+    #     @parent.children << self
+    # end
 end
